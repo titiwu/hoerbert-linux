7f8b77531116c2f19aad7e786dbacc76c34cf09b
Matthias Busl <matthias.busl@googlemail.com>
2019-12-27 21:08:23 +0100

    Add multiprocessing to script
    - Minor addtions to readme
    - Better output + script timing

--- README.md
+++ README.md
@@ -2,18 +2,24 @@
 Helper script for converting audio files for the music player for children
 
 [Hörbert](https://www.hoerbert.com/) is a mp3 player for children which requires Mono-WAV files for energy saving playback. 
-For Windows and Mac graphical tools are provided, for Linux so far only the `sox` command.
+For Windows and Mac graphical tools are provided, for Linux so far only the `sox` command is advised to used (manually).
 
-## mp3toHoerbert 
-Look for subdirectories in the active directory and convert all mp3 and ogg files to WAV for Hoerbert
+## mp3toHoerbert.sh 
+Looks for subdirectories in the active directory and convert all mp3 and ogg files to WAV for Hoerbert
 ```
- - Folder
+ _ Folder
    |_ mp3toHoerbert.sh
    |_ Folder1
    |  |_ Song1.mp3
    |  |_ Song2.ogg
    |_ SDCARD <- created by script
-      |_Folder1
-         |_0.WAV
-         |_1.WAV
+      |_ Folder1
+         |_ 0.WAV
+         |_ 1.WAV
 ```
+
+Usage `# .\mp3toHoerbert.sh`
+
+Rename all folders in the "SDCARD" folder from 0 to (maximum) 8 in the desired order (assigns folders/music to Hoerberts buttons).
+
+Copy all folders to the SD-card an put it into Hoerbert! 
--- mp3toHoerbert.sh
+++ mp3toHoerbert.sh
@@ -3,7 +3,8 @@ DATE=$(date '+%m-%d-%Y')
 TIME=$(date '+%H:%M:%S')
 
 # Start a new file
-echo "Start converting at $DATE $TIME with PID $$"
+start_time=`date +%s`
+echo "INFO: Start converting at $DATE $TIME with PID $$"
 
 # Look for subdirectories and convert all mp3 and ogg files to WAV for Hoerbert
 # - Folder
@@ -18,27 +19,35 @@ echo "Start converting at $DATE $TIME with PID $$"
 
 output_dir=SDCARD
 mkdir -p $output_dir
+
+# Function for converting files
+convert_file () {
+      local file_name=$i
+      local output_file="../${output_dir}/${my_dir}/${new}.WAV"
+      sox --buffer 131072 --multi-threaded --no-glob "$file_name" --clobber -r 32000 -b 16 -e signed-integer --no-glob "${output_file}" remix - gain -n -1.5 bass +1 loudness -1 pad 0 0 dither
+      # If returncode == 0
+      if [ $? -eq 0 ];then
+        echo "INFO: \"${my_dir}/${file_name}\" converted to \"${output_file}\""
+      else
+        echo "WARNING: failed decoding \"${my_dir}/${file_name}\""
+      fi
+}
+
+# Loop over all files
 for my_dir in *; do 
   if [[ -d ${my_dir} && ! ${my_dir} == $output_dir ]]; then 
-    echo Found folder ${my_dir}
+    echo "INFO: Found folder \"${my_dir}\""
     cd "${my_dir}"
     a=0
     # Encode to WAV
     mkdir -p "../$output_dir/${my_dir}"
-    find -iname "*.mp3" -o -iname "*.ogg" | sort -n | while read i; do
-      new=$(printf "%d" "$a")
-      output_file="../${output_dir}/${my_dir}/${new}.WAV"
-      sox --buffer 131072 --multi-threaded --no-glob "$i" --clobber -r 32000 -b 16 -e signed-integer --no-glob "${output_file}" remix - gain -n -1.5 bass +1 loudness -1 pad 0 0 dither
-      # If returncode == 0
-      if [ $? -eq 0 ];then
-        echo "\"${my_dir}/${i}\" converted to \"${output_file}\""
-      else
-        echo "failed decoding \"${my_dir}/${i}\""
-      fi
-      let a=a+1
-    done
+    while read i; do
+        new=$(printf "%d" "$a")
+        convert_file &
+        let a=a+1
+    done < <(find -iname "*.mp3" -o -iname "*.ogg" | sort -n)
     cd ..
-  fi 
+  fi
 done
-
-echo "Finished converting at $DATE $TIME with PID $$"
+wait
+echo "INFO: Finished! Converting took $(expr `date +%s` - $start_time) s"
